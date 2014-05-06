cae36bfd03e1e165d8a203cf5452d2ce111208fa
oliverlim <oliver.lim777@gmail.com>
2014-05-06 09:41:52 +0800

    TVEEZFLIPBOARD

---
+++ flip/script.js
@@ -0,0 +1,28 @@
+$(function(){
+  var mag = $('#magazine');
+  mag.turn();
+  mag.bind('turned', function(e, page, pageObj) {
+    if(page == 1 && $(this).data('done')){
+      mag.addClass('centerStart').removeClass('centerEnd');
+    }
+    else if (page == 32 && $(this).data('done')){
+      mag.addClass('centerEnd').removeClass('centerStart');
+    }
+    else {
+      mag.removeClass('centerStart centerEnd');
+    }
+});
+
+setTimeout(function(){
+  mag.fadeTo(500,1);
+},1000);
+
+$(window).bind('keydown', function(e){
+  if (e.keyCode == 37){
+    mag.turn('previous');
+  }
+  else if (e.keyCode==39){
+    mag.turn('next');
+  }
+  });
+});
